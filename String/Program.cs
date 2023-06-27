9d4c8a0308d46033982ce8a79c52720e8405d28d
Yonan <kochoyev@inbox.ru>
2023-06-27 09:52:44 +0300

    new project

---
+++ String/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Hello";
+            str += " world";
+            Console.WriteLine(str);
+            Console.ReadLine();
+        }
+    }
+}
