e162b1d4b8c6f7a31084d440a1baef4670caa9a9
Yonan <kochoyev@inbox.ru>
2023-06-27 10:12:37 +0300

    new func

--- String/Program.cs
+++ String/Program.cs
@@ -13,6 +13,15 @@ namespace String
             string str = "Hello";
             str += " world";
             Console.WriteLine(str);
+            Console.WriteLine($"это строка {str} "); 
+            Console.WriteLine($"это первый символ {str[0]} ");
+            Console.WriteLine($"это первый символ {str[4]} ");
+            Console.WriteLine($"Длина строки {str.Length} ");
+            int lenght = str.Length;
+            for (int i = 0; i < lenght; i++)
+            {
+                Console.WriteLine($"{i} -> {str[i]}");
+            }
             Console.ReadLine();
         }
     }
